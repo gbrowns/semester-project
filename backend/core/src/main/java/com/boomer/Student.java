87aff70d79e58990a8f9383c3a08c3c4870df18b
Godwin254 <godwinbee15@gmail.com>
2021-02-08 14:23:29 +0300

    add a constructor to the Student class

--- backend/core/src/main/java/com/boomer/Student.java
+++ backend/core/src/main/java/com/boomer/Student.java
@@ -4,6 +4,11 @@ public class Student {
     private String name;
     private int regNo;
 
+    public Student(String name, int regNo){
+        this.name = name;
+        this.regNo = regNo;
+    }
+
     public String getName() {
         return name;
     }

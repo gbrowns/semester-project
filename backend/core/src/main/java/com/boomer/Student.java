81c67b5fe2b6c6959a9be30472014c9bdb2c0818
Godwin254 <godwinbee15@gmail.com>
2021-02-08 14:16:47 +0300

    add getRegNo to STudent class

--- backend/core/src/main/java/com/boomer/Student.java
+++ backend/core/src/main/java/com/boomer/Student.java
@@ -2,8 +2,12 @@ package com.boomer;
 
 public class Student {
     private String name;
+    private int regNo;
 
     public String getName() {
         return name;
+    }
+    public int getRegNo(){
+        return regNo;
     }
 }

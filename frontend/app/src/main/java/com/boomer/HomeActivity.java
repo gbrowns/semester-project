b47df26168d8b5707b47c48c29b4dbae86dd0c46
Godwin254 <godwinbee15@gmail.com>
2021-03-28 21:48:11 +0300

    Modified files in app
    created the promo screen at the home page fragment

--- frontend/app/src/main/java/com/boomer/HomeActivity.java
+++ frontend/app/src/main/java/com/boomer/HomeActivity.java
@@ -1,16 +1,55 @@
 package com.boomer;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomeActivity extends AppCompatActivity {
+import com.boomer.databinding.ActivityHomeBinding;
 
+public class HomeActivity extends AppCompatActivity {
+    ActivityHomeBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment, new HomeFragment())
+                .commit();
+        binding.bottomNavView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.homeFragment:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment, new HomeFragment())
+                            .commit();
+                    break;
+                case R.id.storeFragment:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment, new StoreFragment())
+                            .commit();
+                    break;
+                case R.id.notificationFragment:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment, new NotificationFragment())
+                            .commit();
+                    break;
+                case R.id.settingsFragment:
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment, new SettingsFragment())
+                            .commit();
+                    break;
+                default:
+                    return true;
+            }
+            return true;
+        });
     }
 }

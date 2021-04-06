29de9b2888736f4b86a1a1910b8a28300cf447c7
Godwin254 <godwinbee15@gmail.com>
2021-04-06 14:22:24 +0300

    updated login and sign up functionality

--- frontend/app/src/main/java/com/boomer/LoginActivity.java
+++ frontend/app/src/main/java/com/boomer/LoginActivity.java
@@ -3,9 +3,13 @@ package com.boomer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,10 +17,17 @@ public class LoginActivity extends AppCompatActivity {
     TextView newUserBtn;
     Button loginBtn;
 
+    EditText username;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //get elements by id
+        username = findViewById(R.id.uname);
+        password = findViewById(R.id.passcode);
 
         //set click events
         newUserBtn = findViewById(R.id.newUserBtn);
@@ -28,8 +39,28 @@ public class LoginActivity extends AppCompatActivity {
         //set click events
         loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
-            startActivity(intent);
+
+            //grab text input
+            String gEmail = username.getText().toString();
+            String gPassword = password.getText().toString();
+
+            loginWithEmail(gEmail, gPassword);
+
+        });
+    }
+
+    public void loginWithEmail(String email ,String password){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(it->{
+            if (it.isSuccessful()){
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            else{
+                String error = it.getException().getLocalizedMessage();
+                Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
--- frontend/app/src/main/java/com/boomer/SingupActivity.java
+++ frontend/app/src/main/java/com/boomer/SingupActivity.java
@@ -3,12 +3,15 @@ package com.boomer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -40,11 +43,11 @@ public class SingupActivity extends AppCompatActivity {
             regPhoneNo = findViewById(R.id.phoneNumber);
             regPassword = findViewById(R.id.passcode);
 
+            rootNode = FirebaseDatabase.getInstance("https://boomer-instruments-app-default-rtdb.firebaseio.com/");
             //save data to firebase on user sign up
             regBtn.setOnClickListener(v -> {
 
                 //rootNode get database instance
-                rootNode = FirebaseDatabase.getInstance("https://boomer-instruments-app-default-rtdb.firebaseio.com/");
                 reference = rootNode.getReference("users"); //set reference location to users
 
 
@@ -60,19 +63,11 @@ public class SingupActivity extends AppCompatActivity {
                 String phoneNo = regPhoneNo.getText().toString();
                 String password = regPassword.getText().toString();
 
-                UserHelperClass helperClass = new UserHelperClass(name, email, phoneNo, password); //object instance of userHelperClass
+                //UserHelperClass helperClass = new UserHelperClass(name, email, phoneNo, password); //object instance of userHelperClass
 
-                reference.child(phoneNo).setValue(helperClass); //uniquely sets values to DB
+                //reference.child(phoneNo).setValue(helperClass); //uniquely sets values to DB
+                    signUpWithEmail(email, password, phoneNo, name);
 
-                //clear input fields
-                regName.setText("");
-                regEmail.setText("");
-                regPhoneNo.setText("");
-                regPassword.setText("");
-
-                //switch to the homepage activity
-                Intent intent = new Intent(SingupActivity.this, HomeActivity.class);
-                startActivity(intent);
 
             }); //signupButton method ends
 
@@ -162,4 +157,53 @@ public class SingupActivity extends AppCompatActivity {
         }
     } //validate password input
 
+
+    private void signUpWithEmail(String email, String password, String phoneNo, String name) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(it ->{
+            if (it.isSuccessful()){
+                if (auth.getCurrentUser().getUid() != null){
+                    String uid = auth.getCurrentUser().getUid();
+                    rootNode.getReference("users/"+ uid).setValue(new UserHelperClass(phoneNo, name)).addOnCompleteListener(task->{
+                        if (task.isSuccessful()){
+                            //signup complete
+                            //clear input fields
+                            regName.setText("");
+                            regEmail.setText("");
+                            regPhoneNo.setText("");
+                            regPassword.setText("");
+
+                            //switch to the homepage activity
+                            Intent intent = new Intent(SingupActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            String error = task.getException().getLocalizedMessage();
+                            Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+            else{
+                String error = it.getException().getLocalizedMessage();
+                Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+    /*
+    private void validateData(){
+        String name = regName.getText().toString();
+        String email = regEmail.getText().toString();
+        if (name.length() < 1){
+            //empty name
+            return;
+        }
+        if (email.length() < 1 || !Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //email invalid
+            return;
+        }
+    }
+
+     */
 }
